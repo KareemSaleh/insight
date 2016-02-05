797279873b21f7aa49f48c92534cbe5bd7fc6150
Kareem Saleh <kareem@tribehr.com>
2016-02-05 14:10:19 -0500

    No need to make the vagrant provision so verbose.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,34 +1,31 @@
 #!/usr/bin/env bash
 
 # Install Apache, PHP, MySQL
-sudo apt-get update
-sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client mcrypt
-
-# Get rid of Apache warnings
-# echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
-
+echo "Installing LAMP stack..."
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mcrypt php5-curl php5-mysql php5-gd php5-cli php5-dev mysql-client mcrypt > /dev/null 2>&1
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant/cakephp-2.7.9 /var/www
 fi
 
-#Configure MySQL Server
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123'
+# Configure MySQL Server
+echo "Configuring MySQL Server..."
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password MYPASSWORD123' > /dev/null 2>&1
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password MYPASSWORD123' > /dev/null 2>&1
 
-sudo apt-get -y install mysql-server
+sudo apt-get -y install mysql-server > /dev/null 2>&1
+
 
 #Restart all the installed services to verify that everything is installed properly
-echo -e "\n"
+echo -e "Verifying and Starting services..."
 
-service apache2 restart && service mysql restart > /dev/null
-
-echo -e "\n"
+echo "ServerName 127.0.0.1" >> /etc/apache2/apache2.conf
+service apache2 restart > /dev/null 2>&1
+service mysql restart > /dev/null 2>&1
 
 if [ $? -ne 0 ]; then
-   echo "Error: Please Check the Installed Services, Installed services failed to run."
+   echo "Error: Please Check the Installed Services, Installed services failed to run.\n"
 else
-   echo "Installed Services ran Sucessfully!"
+   echo "Installed Services ran Sucessfully!\n"
 fi
-
-echo -e "\n"
